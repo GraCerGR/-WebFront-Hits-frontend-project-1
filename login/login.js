b20b98a40a9eec99fa2ba727eb21d0ec57714341
Grigoriy Sribnyy <ctrhtnysq.afqk@gmail.com>
2023-11-05 00:49:13 +0700

    Post запрос для входа сделан Получаем токен

--- login/login.js
+++ login/login.js
@@ -1,14 +1,30 @@
-async function post(url, data=null){
-    return fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: new Headers({
-            'Content-Type' : 'application/json'
-        })
-    }).then(response => response.json()).then(data => {console.log(data)});
+async function post(url, data = null) {
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (data.token) {
+        // Если успех, получаем токен из ответа
+        const token = data.token;
+        // Далее можно выполнять другие действия с полученным токеном
+        // Например, сохранить его в localStorage и перенаправить пользователя на другую страницу
+      } else {
+        // Если ошибка, выводим сообщение об ошибке
+        alert(data.message || 'Неверный логин или пароль');
+      }
+    })
+    .catch(error => {
+      console.error('Ошибка', error);
+    });
 }
   
-  async function get(url, token) {
+ /* async function get(url, token) {
     return fetch(url, {
       method: 'GET',
       headers: new Headers({
@@ -23,7 +39,7 @@ async function post(url, data=null){
     .catch(error => {
       console.error('Ошибка', error);
     });
-  }
+  }*/
   
   const url = "https://food-delivery.kreosoft.ru/api/account/login";
   post(url);
